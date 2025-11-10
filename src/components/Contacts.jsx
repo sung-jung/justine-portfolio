a9e96a4b03de68567b844da192518b7980630b6c
sung-jung <csbs77570@gmail.com>
2025-11-10 13:22:07 +0800

    updated the resume link

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
             </form>
 
             <div className="contact-links">
-                <a href="https://drive.google.com/file/d/1CziGEX7LnzYfz73oizEyuJzaRhbzcFNX/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1_me04CGnEwHC_6QKxgt74RE4-HH6Rgal/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <FaFileAlt /> View Resume
                 </a>
                 <a href="mailto:mendoza262003@gmail.com" target="_blank" rel="noopener noreferrer">
