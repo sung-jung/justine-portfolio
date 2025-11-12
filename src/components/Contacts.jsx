b18885e77110df718988b1d9a332af2db8ac249f
sung-jung <csbs77570@gmail.com>
2025-11-12 21:56:14 +0800

    Added Certifications section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
             </form>
 
             <div className="contact-links">
-                <a href="https://drive.google.com/file/d/1k7r4OTR3bzwzHeFHpzf94Lu4fEnmpzW7/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1aY4WyY9E31GRDTJMIAMxogG53UBUJaiF/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <FaFileAlt /> View Resume
                 </a>
                 <a href="mailto:mendoza262003@gmail.com" target="_blank" rel="noopener noreferrer">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,18 +17,54 @@ const Projects = () => {
         },
     ];
 
+    const certifications = [
+        {
+            title: "CCNAv7: Introduction to Networks",
+            date: "Oct 2, 2024",
+            issuer: "CISCO Networking Academy",
+            link: "https://drive.google.com/file/d/1xzsowDdfh7rMzYt2QJ3Fp6gGkbpCmf-_/view?usp=drive_link"
+        },
+        {
+            title: "Cybersecurity Essentials ",
+            date: "Oct 2, 2024",
+            issuer: "CISCO Networking Academy",
+            link: "https://drive.google.com/file/d/1Z_kA6Eo6hh0pCa_h7oLJGT9CMdo2u7E-/view?usp=drive_link"
+        },
+        {
+            title: "Cyber Threat Management Course",
+            date: "Oct 29, 2024",
+            issuer: "CISCO Networking Academy",
+            link: "https://drive.google.com/file/d/1X8HVIanpRDgRVxWO9u4_ng3mX2KwHu8b/view?usp=drive_link"
+        }
+    ]
+
     return (
         <section className="projects-section" id="projects">
-            <h2 className="section-title">Projects</h2>
+            <h2 className="section-title">Projects and Certifications</h2>
+
+            <h3 className="sub-title">Projects</h3>
             <div className="projects-container">
                 {projectList.map((project, index) => (
                     <div className="project-card" key={index}>
-                        <h3>{project.title}</h3>
-                        <h4>{project.role}</h4>
+                        <h4>{project.title}</h4>
+                        <h5>{project.role}</h5>
                         <p>{project.description}</p>
                     </div>
                 ))}
             </div>
+            <h3 className="sub-title">Certifications</h3>
+            <div className="certifications-container">
+                {certifications.map((cert, index) => (
+                    <div className="cert-card" key={index}>
+                        <a href={cert.link} target="_blank" rel="noopener noreferrer">
+                            <h4>{cert.title}</h4>
+                            <p>{cert.issuer} | {cert.date}</p>
+                        </a>
+
+                    </div>
+                ))}
+            </div>
+
         </section>
     );
 };
